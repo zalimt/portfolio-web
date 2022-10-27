7e550f53df1f115dd16124e6fa80ff5c6b761b40
ztsorionov@oneroyal.com <ztsorionov@oneroyal.com>
2022-10-27 16:34:19 +0300

    third

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <HeaderMain />
-  <Carousel>
-    <Slide v-for="(slide, index) in works" :key="index">
-
+<div class="home">
+  <Carousel class="carousel"  v-slot="{ currentSlide }" >
+    <Slide v-for="(slide, index) in carouselSlides" :key="index">
+      <div v-show="currentSlide === index + 1" class="slide-info">
+          <img :src="require(`./assets/${slide.name}.png`)" :alt="slide.webSiteName">
+          <a class="site-url" :href="slide.siteUrl" target="_blank">VIEW LIVE</a>
+      </div>
     </Slide>
   </Carousel>
+</div>
+  
  
 
 </template>
@@ -23,19 +29,63 @@ export default {
     Slide
   },
   setup() {
-    const works = ["ws-1", "ws-2", "ws-3", "ws-4", "ws-5", "ws-6", "ws-7", "ws-8"];
-
-    return works
-  },
-  data(){
-    return {
-      
-    }
+    // const carouselSlides = ["ws-1", "ws-2", "ws-3", "ws-4", "ws-5", "ws-6", "ws-7", "ws-8"];
+    const carouselSlides = [
+      {
+        name: "ws-1",
+        webSiteName: "mysmartswimwear",
+        siteUrl: "//mysmartswimwear.com/"
+      },
+      {
+        name: "ws-2",
+        webSiteName: "cryptalo",
+        siteUrl: "//cryptalo.com/"
+      },
+      {
+        name: "ws-3",
+        webSiteName: "digikeymarket",
+        siteUrl: "//digikeymarket.com/"
+      },
+      {
+        name: "ws-4",
+        webSiteName: "investmedia",
+        siteUrl: "//investmedia.tech/"
+      },
+      {
+        name: "ws-5",
+        webSiteName: "fxbrokerrating",
+        siteUrl: "//fxbrokerrating.com/"
+      },
+      {
+        name: "ws-6",
+        webSiteName: "trendingbrokers",
+        siteUrl: "//trendingbrokers.com/"
+      },
+      {
+        name: "ws-7",
+        webSiteName: "guiadeltrading",
+        siteUrl: "//guiadeltrading.com/"
+      },
+      {
+        name: "ws-8",
+        webSiteName: "forexinspect",
+        siteUrl: "//forexinspect.com/"
+      },
+    ]
+    return { carouselSlides }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -46,7 +96,6 @@ export default {
 }
 
 .container {
-  /* width: 100%; */
   max-width: 1366px;
   margin: auto;
   padding: 0 40px;
@@ -66,5 +115,60 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-between;
+}
+
+.carousel {
+  position: relative;
+  height: auto;
+
+  .slide {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .slide-info {
+      width: 100%;
+      max-width: 850px;
+      height: auto;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+
+      .site-url {
+        // color: #FFD600;
+        position: relative;
+        color: #333;
+        text-decoration: none;
+        padding: 10px 15px;
+        font-weight: 500;
+        border: 1px solid #333;
+        z-index: 0;
+
+        &::before {
+          content: "";
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
+          background: #fff;
+          z-index: -1;
+          transition: .4s all;
+        }
+
+        &:hover::before {
+          background-color: #333;
+        }
+
+        &:hover {
+          transition: .4s all;
+          color: #FFD600;
+        }
+      }
+    }
+  }
 }
 </style>
--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,15 +1,94 @@
 <template>
   <div class="carousel">
-    <slot />
+    <slot :currentSlide="currentSlide"/>
+    <!-- Navigation -->
+    <div class="navigate">
+        <div class="toggle-page left">
+            <i @click="prevSlide" class="fas fa-chevron-left"></i>
+        </div>
+        <div class="toggle-page right">
+            <i @click="nextSlide" class="fas fa-chevron-right"></i>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { ref, onMounted } from "vue"
+
 export default {
     name: "CarouselComp",
+    setup() {
+      const currentSlide = ref(1);
+      const slideCount = ref(null);
+
+      // Previous Slide
+      const prevSlide = () => {
+        if (currentSlide.value === slideCount.value - slideCount.value + 1) {
+          currentSlide.value = slideCount.value;
+          return;
+        }
+        currentSlide.value -= 1;
+      };
+
+      // Next Slide
+      const nextSlide = () => {
+        if (currentSlide.value === slideCount.value) {
+          currentSlide.value = 1
+          return;
+        }
+        currentSlide.value += 1;
+      };
+
+      // Slide Count
+      onMounted(() => {
+        slideCount.value = document.querySelectorAll(".slide").length;
+      });
+
+      return { currentSlide, prevSlide, nextSlide, slideCount }
+    }
+
+    
 }
 </script>
 
-<style>
+<style lang="scss">
+
+.navigate {
+  position: absolute;
+  top: 50%;
+  padding: 1px;
+  height: auto;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .toggle-page {
+    &.left {
+      margin-right: 800px;
+    }
+
+      i {
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 40px;
+          height: 40px;
+          background-color: #333;
+          color: #FFD600;
+      }
+  }
+  
+  @media (max-width: 1000px) {
+    .toggle-page {
+      &.left {
+        margin-right: 300px;
+      }
+    }
+  }
+}
+
 
 </style>
